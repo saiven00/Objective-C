6b90f1f224b08b9f736bb7c059687dd0d10f998e
Liushaoyi <mailshaoyi@163.com>
2017-06-15 15:10:23 +0800

    初始化工程

---
+++ XAHealth/XAHealth/Main/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XAHealth
+//
+//  Created by 刘少毅 on 2017/6/15.
+//  Copyright © 2017年 XingYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XAHealth/XAHealth/Supporting Files/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XAHealth
+//
+//  Created by 刘少毅 on 2017/6/15.
+//  Copyright © 2017年 XingYang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
